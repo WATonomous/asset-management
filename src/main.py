53cca4e0ebec47001055d18ef0c13a473274dde3
Ben Zhang <ben.zhang@watonomous.ca>
2024-12-06 06:30:30 +0000

    Run set_up_logging in main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import json
 import os
 from watcloud_utils.typer import app
+from watcloud_utils.logging import set_up_logging
 
 from .agent import Agent
 
 WORKSPACE_DIR = os.getenv("WORKSPACE_DIR", "/tmp/workspace")
+
+set_up_logging()
 
 @app.command()
 def run_agent():
